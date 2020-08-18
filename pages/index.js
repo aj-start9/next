efdd91b3505e59b6ab9d959be729b4ae0159acae
ankit9678 <ankit.jain@opscale.io>
2020-08-18 10:06:39 +0530

    add7

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,12 @@ import cookies from 'nookies'
 const Home = () => null
 
 Home.getInitialProps = context => {
-  const {country} = cookies.get(context)
+  const { country } = cookies.get(context)
+
+  const test = context.query.country || country || 'us'
   process.browser ?
-    Router.replace('/[country]', `${country}`)
-    : context.res.writeHead(302, { Location: `/${country}` }).end()
+    Router.replace('/[country]', `${test}`)
+    : context.res.writeHead(302, { Location: `/${test}` }).end()
 }
 
 export default Home
